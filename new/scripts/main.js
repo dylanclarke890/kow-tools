860083c6b8191777705054fc8402bcf97ba12ad7
dylanclarke890 <dylanclarke890@gmail.com>
2022-11-16 23:19:06 +0000

    replaced navbar with a less nested version. Does not account for mobile currently

--- new/scripts/main.js
+++ new/scripts/main.js
@@ -7,6 +7,12 @@ const pages = [
 ];
 
 const navTab = (pageInfo, active) => `
+  <div>
+    <a ${active ? "class='active' href='#'" : `href="${pageInfo.url}.html"`}">${pageInfo.title}</a>
+  </div>
+  `;
+
+const oldnavTab = (pageInfo, active) => `
     <li class="nav-item">
       <a class="nav-link${active ? ' active" href="#wrapper"' : `" href="${pageInfo.url}.html"`}">${
   pageInfo.title
@@ -16,7 +22,17 @@ const navTab = (pageInfo, active) => `
 
 const url = window.location.href;
 const currentLocation = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
+
 const navbar = `
+<nav id="navbar">
+  <a href="index.html">
+    <img id="logo" src="images/logo1_50perc.png" alt="logo" />
+  </a>
+  ${pages.map((p) => navTab(p, p.url === currentLocation)).join("")}
+</nav>
+`;
+
+const oldNavbar = `
   <nav id="banner" class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid" id="menu">
       <button
@@ -29,7 +45,7 @@ const navbar = `
       <a class="navbar-brand" id="logo-container" href="index.html">
         <img id="logo" src="images/logo1_50perc.png" alt="logo" />
       </a>
-      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <div class="collapse navbar-collapse justify-content-center">
         <ul id="menuList" class="nav navbar-nav justify-content-center">
         ${pages.map((p) => navTab(p, p.url === currentLocation)).join("")}
         </ul>
